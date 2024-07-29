b3aa0fd3756dcde262b2dce21598172179ba0739
kdiggety <wakes-pillory.01@icloud.com>
2024-07-28 17:14:20 -0700

    refactoring

--- graphql-server/server.js
+++ graphql-server/server.js
@@ -1,7 +1,7 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
-const { resolvers } = require('../graphql-server/resolver')
-const { typeDefs } = require('../graphql-server/schema')
+const { resolvers } = require('./resolver')
+const { typeDefs } = require('./schema')
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
